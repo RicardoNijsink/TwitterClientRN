860c91e1ae4ecf4a3473117782c732a29cc5bdab
RicardoNijsink <ricardonijsink@gmail.com>
2015-05-02 11:35:10 +0200

    Project aangemaakt

---
+++ ProjectTwitterClient/src/nl/saxion/rn/projecttwitterclient/User.java
@@ -0,0 +1,14 @@
+package nl.saxion.rn.projecttwitterclient;
+
+import java.net.URL;
+
+public class User {
+	private String createdAt, description, location, name;
+	private int followersCount, friendsCount;
+	private URL profileImage;
+	
+	public User(String createdAt) {
+		
+	}
+
+}
